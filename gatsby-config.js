aafe2ba8d3f687f4d0fd5be23e971dcb2dba67c4
Mamik Das <30488855+Mamik153@users.noreply.github.com>
2020-07-09 00:34:26 +0530

    Added gatsby-google-fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,17 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono`,
+          `muli\:400,400i,700,700i`,
+          `open sans\:300,400,400i,700`,
+          `raleway`
+        ],
+        display: "swap",
+      },
+    },
   ],
 }
